07a498900ac7833b1a8c433b1d0e547761172c64
marudhupandiyang <marudhupandiyang@gmail.com>
2013-03-27 10:39:48 +0530

    Generate model

--- lib/generators/pigeonpost/pigeonpost_generator.rb
+++ lib/generators/pigeonpost/pigeonpost_generator.rb
@@ -1,10 +1,20 @@
 class PigeonpostGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
 
-  attr = ['subject,message,sender,receiver,senderdelete,receiverdelete']
+  model_attr = ['subject:text,message:text,sender:user,receiver:user,senderdelete:datetime,receiverdelete:datetime']
 
-  argument :model_name ,:type => :string , :default => :user
-	argument :atributes  ,:type => :array  , :default => attr
+  argument :model_name ,:type => :string , :default => 'user'
+	argument :atributes  ,:type => :array  , :default => model_attr.join(',')
+
+	def generate_model
+		#check if the class exist
+		if defined?(User)
+
+		else
+			generate(:user,model_attr.join(','))
+		end
+
+	end
 
 
 
