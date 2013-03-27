456cda0d3f1aa6512e099dedee8e3526b4ff1e19
marudhupandiyang <marudhupandiyang@gmail.com>
2013-03-27 10:30:32 +0530

    Changed usage and arguments

--- lib/generators/pigeonpost/pigeonpost_generator.rb
+++ lib/generators/pigeonpost/pigeonpost_generator.rb
@@ -1,3 +1,11 @@
 class PigeonpostGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
+
+  attr = ['subject,message,sender,receiver,senderdelete,receiverdelete']
+
+  argument :model_name ,:type => :string , :default => :user
+	argument :atributes  ,:type => :array  , :default => attr
+
+
+
 end
