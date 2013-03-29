ba951641495772b1de77e20da94dc4d0ec468d03
marudhupandiyang <marudhupandiyang@gmail.com>
2013-03-30 00:31:53 +0530

    Generator correction

--- lib/generators/pigeonpost/pigeonpost_generator.rb
+++ lib/generators/pigeonpost/pigeonpost_generator.rb
@@ -1,7 +1,7 @@
 class PigeonpostGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
 
-  @@model_attr = ['subject:text', 'message:text', 'sender:user', 'receiver:user' , 'senderdelete:datetime' ,  'receiverdelete:datetime']
+  @@model_attr = ['subject:text', 'message:text', 'sender:integer', 'receiver:integer' , 'senderdelete:datetime' ,  'receiverdelete:datetime']
 
 
   argument :model_name ,:type => :string , :default => 'user' 
